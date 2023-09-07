c261825a8fe639bc138199e0d1c35f0504be6f24
vhickjr <victormathias001@gmail.com>
2023-09-07 17:15:07 +0100

    changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Skeleton from "react-loading-skeleton";
 import { Link, useParams } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 import { useDispatch } from "react-redux";
@@ -13,7 +12,7 @@ const Product = () => {
   const [loading, setLoading] = useState(false);
   const [item, setItem] = useState(Data);
 
-  const menuItems = [...new Set(Data.map((Val) => Val.category))];
+  const selectedProduct = item.find((Val) => Val.id === id);
 
   const filterItem = (curcat) => {
     const newItem = Data.filter((newVal) => {
@@ -28,74 +27,7 @@ const Product = () => {
     dispatch(addCart(product));
   };
 
-  const Loading = () => {
-    return (
-      <>
-        <div className="container my-5 py-2">
-          <div className="row">
-            <div className="col-md-6 py-3">
-              <Skeleton height={400} width={400} />
-            </div>
-            <div className="col-md-6 py-5">
-              <Skeleton height={30} width={250} />
-              <Skeleton height={90} />
-              <Skeleton height={40} width={70} />
-              <Skeleton height={50} width={110} />
-              <Skeleton height={120} />
-              <Skeleton height={40} width={110} inline={true} />
-              <Skeleton className="mx-3" height={40} width={110} />
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
-  const ShowProduct = () => {
-    return (
-      <>
-        <div className="container my-5 py-2">
-          <div className="row">
-            <div className="col-md-6 col-sm-12 py-3">
-              <img
-                className="img-fluid"
-                src={product.image}
-                alt={product.title}
-                width="400px"
-                height="400px"
-              />
-            </div>
-            <div className="col-md-6 col-md-6 py-5">
-              <h4 className="text-uppercase text-muted">{product.category}</h4>
-              <h1 className="display-5">{product.title}</h1>
-              <p className="lead">
-                {product.rating && product.rating.rate}{" "}
-                <i className="fa fa-star"></i>
-              </p>
-              <h3 className="display-6  my-4">${product.price}</h3>
-              <p className="lead">{product.description}</p>
-              <button
-                className="btn btn-outline-dark"
-                onClick={() => addProduct(product)}
-              >
-                Add to Cart
-              </button>
-              <Link to="/cart" className="btn btn-dark mx-3">
-                Go to Cart
-              </Link>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
   const Card = ({ item }) => {
-    const dispatch = useDispatch();
-
-    const addProduct = (item) => {
-      dispatch(addCart(item));
-    };
     return (
       <>
         <div className="card">
@@ -146,7 +78,40 @@ const Product = () => {
     <>
       <Navbar />
       <div className="container">
-        <div className="row">{loading ? <Loading /> : <ShowProduct />}</div>
+        <div className="row">
+          <div className="container my-5 py-2">
+            <div>
+              <div key={selectedProduct.id}>
+                <div className="col-md-6 col-sm-12 py-3">
+                  <img
+                    className="img-fluid"
+                    src={selectedProduct.img}
+                    alt={selectedProduct.title}
+                    width="400px"
+                    height="400px"
+                  />
+                </div>
+                <div className="col-md-6 col-md-6 py-5">
+                  <h4 className="text-uppercase text-muted">
+                    {selectedProduct.category}
+                  </h4>
+                  <h1 className="display-5">{selectedProduct.title}</h1>
+                  <h3 className="display-6 my-4">${selectedProduct.price}</h3>
+                  <p className="lead">{selectedProduct.desc}</p>
+                  <button
+                    className="btn btn-outline-dark"
+                    onClick={() => addProduct(selectedProduct)}
+                  >
+                    Add to Cart
+                  </button>
+                  <Link to="/cart" className="btn btn-dark mx-3">
+                    Go to Cart
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row my-5 py-5">
           <div className="d-none d-md-block">
             <h2 className="">You may also Like</h2>
