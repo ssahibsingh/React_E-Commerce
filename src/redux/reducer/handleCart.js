5a447e75d6e448ef53e391be5f25f38f6bd27027
ssahibsingh <snone181@gmail.com>
2024-09-12 12:39:07 +0530

    resolved #5: products get retained in the cart

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -1,33 +1,79 @@
-const cart = []
+// const cart = []
 
-const handleCart = (state=cart, action) =>{
-    const product = action.payload
-    switch(action.type){
-        case "ADDITEM":
-            // Check if product already in cart
-            const exist = state.find((x) => x.id === product.id)
-            if(exist){
-                // Increase the quantity
-                return state.map((x)=>x.id ===product.id?{...x, qty: x.qty+1}:x)
-            }
-            else{
-                return [...state, {...product, qty:1}]
-            }
-            break;
-        case "DELITEM":
-            const exist2 = state.find((x) => x.id === product.id)
-            if(exist2.qty === 1){
-                return state.filter((x)=>x.id!==exist2.id)
-            }
-            else{
-                return state.map((x)=> x.id===product.id?{...x, qty:x.qty-1}:x)
-            }
-            break;
+// const handleCart = (state=cart, action) =>{
+//     const product = action.payload
+//     switch(action.type){
+//         case "ADDITEM":
+//             // Check if product already in cart
+//             const exist = state.find((x) => x.id === product.id)
+//             if(exist){
+//                 // Increase the quantity
+//                 return state.map((x)=>x.id ===product.id?{...x, qty: x.qty+1}:x)
+//             }
+//             else{
+//                 return [...state, {...product, qty:1}]
+//             }
+//             break;
+//         case "DELITEM":
+//             const exist2 = state.find((x) => x.id === product.id)
+//             if(exist2.qty === 1){
+//                 return state.filter((x)=>x.id!==exist2.id)
+//             }
+//             else{
+//                 return state.map((x)=> x.id===product.id?{...x, qty:x.qty-1}:x)
+//             }
+//             break;
 
-        default:
-            return state
-            break;
-    }
-}
+//         default:
+//             return state
+//             break;
+//     }
+// }
 
-export default handleCart
+// export default handleCart
+
+// Retrieve initial state from localStorage if available
+const getInitialCart = () => {
+  const storedCart = localStorage.getItem("cart");
+  return storedCart ? JSON.parse(storedCart) : [];
+};
+
+const handleCart = (state = getInitialCart(), action) => {
+  const product = action.payload;
+  let updatedCart;
+
+  switch (action.type) {
+    case "ADDITEM":
+      // Check if product already in cart
+      const exist = state.find((x) => x.id === product.id);
+      if (exist) {
+        // Increase the quantity
+        updatedCart = state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+        );
+      } else {
+        updatedCart = [...state, { ...product, qty: 1 }];
+      }
+      // Update localStorage
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      return updatedCart;
+
+    case "DELITEM":
+      const exist2 = state.find((x) => x.id === product.id);
+      if (exist2.qty === 1) {
+        updatedCart = state.filter((x) => x.id !== exist2.id);
+      } else {
+        updatedCart = state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+        );
+      }
+      // Update localStorage
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      return updatedCart;
+
+    default:
+      return state;
+  }
+};
+
+export default handleCart;
